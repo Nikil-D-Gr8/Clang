e840e38894a4931ebbb785432329273f9b06c7c1
Nikil_D_Gr8 <snikilpaul@gmail.com>
2024-04-14 23:50:54 +0530

    update(main) : Logical and relational operations have been updated

--- 011 RelationalOperations.c
+++ 011 RelationalOperations.c
@@ -1,7 +1,30 @@
 #include<stdio.h>
 
-
+// Relational operations return boolean values
 void main()
 {
-    
+    int i = 100;
+    int n = 200;
+    int m = 200;
+    int o = 300;
+    if (i < n) 
+        {
+            printf("%d < %d \n",i,n);
+        }
+    if (o > n) 
+        {
+            printf("%d > %d \n",o,n);
+        }
+    if (m == n) 
+        {
+            printf("%d = %d \n",m,n);
+        }
+    if (i <= n) 
+        {
+            printf("%d <= %d \n",i,n);
+        }
+    if (o >= n) 
+        {
+            printf("%d >= %d \n",o,n);
+        }
 }
--- 012 LogicalOperations.c
+++ 012 LogicalOperations.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 
-
+// Generate boolean values based on condition
 void main()
 {
-    
+    int num = 30;
+    // && serves as logical AND operation
+    if ((num % 2 == 0) && (num % 3 == 0))
+    {
+        printf("The number is divisible by 6  \n");
+    } 
+    // || serves as logical OR operation
+    if ((num == 0) || (num > 0))
+    {
+        printf("The number is might be positive or zero ");
+    }
 }
