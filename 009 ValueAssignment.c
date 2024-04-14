b7ce8420eebc02b25ec3b8a8b6c6511dcd106cf1
Nikil_D_Gr8 <snikilpaul@gmail.com>
2024-04-14 22:23:27 +0530

    feat(main) - added arthimetic operations

--- 009 ValueAssignment.c
+++ 009 ValueAssignment.c
@@ -2,10 +2,22 @@
 
 void main()
 
-{
+{   // I) Assigning type and  values seperately 
     int x;
     x = 10 ;
+    
+    // II ) Assigning type and value together
     int y = 20 ;
-    printf(" The two variables are %d and %d",x,y);
-    // you can either assign type seperately and then assign value or do both at the same time
-}
+    printf(" The two variables are %d and %d\n",x,y);
+
+    // III ) Assigning types together and values also together for multipe variables
+    float m,n;
+    m = n = 8.00 ;
+    printf(" The two new variables are %.3f and %.2f\n",m,n);
+
+    // IV ) assigning types and values at the same time for multiple variables
+
+    int i , l = 7;
+    printf(" The values are %d and %d\n",i,l);
+    
+    }
---
+++ 010 ArithemticOperations.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main() {
+    // Addition
+    int add = 5 + 3;
+    printf("Addition: %d\n", add);
+
+    // Subtraction
+    int subtract = 7 - 4;
+    printf("Subtraction: %d\n", subtract);
+
+    // Multiplication
+    int multiply = 6 * 9;
+    printf("Multiplication: %d\n", multiply);
+
+    // Division
+    float divide = 10.0 / 3.0;
+    printf("Division: %.2f\n", divide);
+
+    // Modulus
+    int modulus = 11 % 4;
+    printf("Modulus: %d\n", modulus);
+}
