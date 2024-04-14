425db30576d71ae3cbff3f5447a727b396c2f968
Nikil_D_Gr8 <snikilpaul@gmail.com>
2024-04-14 21:46:28 +0530

    feat(main): added value assignment file

---
+++ 009 ValueAssignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+
+{
+    int x;
+    x = 10 ;
+    int y = 20 ;
+    printf(" The two variables are %d and %d",x,y);
+    // you can either assign type seperately and then assign value or do both at the same time
+}
