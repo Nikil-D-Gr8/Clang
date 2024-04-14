c040ffc3e637332c6f1151945c38a1bfc99aac8c
Nikil_D_Gr8 <snikilpaul@gmail.com>
2024-04-15 00:00:57 +0530

    feat(main) : constants

---
+++ 014 constant.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// we can declare the constant in the begining itself
+#define c 13
+
+
+int main()
+{
+// or define it ula kooda
+    int const d = 4;
+    printf("The two strings are %d and %d",c,d);
+}
