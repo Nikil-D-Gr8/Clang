1fdb786e2450ae4efa5d05f334dfd4e543f3692e
Nikil_D_Gr8 <snikilpaul@gmail.com>
2024-04-14 23:25:19 +0530

    updated(main) Typecasting

--- 013 Typecasting.c
+++ 013 Typecasting.c
@@ -1,18 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
-
-void main()
-{
+void main() {
+    // Explicit type casting
     int x = 5;
     float p = 3.14;
     int ar;
-    // we type cased by basically multiplyin with the data type
-    ar = (int)(p*x*x);
+    ar = (int)(p * x * x); // Explicit type casting
+    printf("The area is %d.\n", ar);
 
-    printf("The area is %d .\n",ar);
+    double pe;
+    pe = 2.0 * (double)p * (double)x;
+    printf("The perimeter is %f.\n", pe);
 
-    double pe ;
-    pe = 2.0*(double)p*(double)x;
-
-    printf("The perimeter is %f .",pe);   
+    // Implicit type casting
+    int num_int = 10;
+    float num_float;
+    num_float = num_int; // Implicit type casting
+    printf("Implicit type casting: %f\n", num_float);
 }
